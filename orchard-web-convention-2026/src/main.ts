49d433387b37581e5bd0be8c961aaa89d7c4131f
chenry22 <112424609+chenry22@users.noreply.github.com>
2026-04-08 16:42:16 -0500

    final updates

--- orchard-web-convention-2026/src/main.ts
+++ orchard-web-convention-2026/src/main.ts
@@ -6,32 +6,28 @@ import Sigma from "sigma";
 let ROTATION_SPEED = 0.0015;
 
 const graph = new Graph();
-graph.addNode("p1", { label: 'hi', x: -3, y: 0, size: 20, color: 'blue' });
-graph.addNode("p2", { x: -1, y: 2, size: 20, color: 'red' });
-graph.addNode("p3", { x: 1, y: 4, size: 20, color: 'green' });
-graph.addNode("p4", { x: 3, y: 2, size: 20, color: 'yellow' });
-graph.addNode("p5", { x: 1, y: -4, size: 20, color: 'orange' });
-graph.addNode('center', { x: 0, y: 0, size: 10, color: 'yellow' });
+graph.addNode("convention", { label: 'April 11th. 11am-1pm', x: -3, y: 0, size: 20, color: '#fff59e' });
+graph.addNode("insta", { label: 'Reparative Computing...', x: -1, y: 2, size: 20, color: '#00d973' });
+graph.addNode("horse", { label: '🐎', x: 1, y: 4, size: 20, color: '#94ff94' });
+graph.addNode("orchard", { label: 'Join the ORCHARD...', x: 3, y: 2, size: 20, color: '#008aa1' });
+graph.addNode('center', { x: 0, y: 0, size: 10, color: '#ffe600' });
 graph.setNodeAttribute('center', "highlighted", true);
 
-graph.addEdge("p1", "p2", { type: 'curved' });
-graph.addEdge("p2", "p3", { type: 'curved' });
-graph.addEdge("p3", "p4", { type: 'curved' });
-graph.addEdge("p4", "p5", { type: 'curved' });
-graph.addEdge("p5", "p1", { type: 'curved' });
+graph.addEdge("convention", "insta", { type: 'curved', color: 'gray' });
+graph.addEdge("insta", "horse", { type: 'curved', color: 'gray' });
+graph.addEdge("horse", "orchard", { type: 'curved', color: 'gray' });
+graph.addEdge("orchard", "convention", { type: 'curved', color: 'gray' });
+
+// graph.addEdge("p1", "p2", { type: 'curved' });
+// graph.addEdge("p2", "p3", { type: 'curved' });
+// graph.addEdge("p3", "p4", { type: 'curved' });
+// graph.addEdge("p4", "p5", { type: 'curved' });
+// graph.addEdge("p5", "p1", { type: 'curved' });
 
 for (let node of graph.nodes()) {
   if (node !== 'center') {
     graph.addEdge("center", node, { size: 0, color: 'black' });
   }
-}
-const img = new Image();
-img.src = "/assets/tree.png";
-img.onload = () => {
-  graph.setNodeAttribute('center', "renderer", (context, data, _) => {
-    context.drawImage(img, data.x - 15, data.y - 15, 30, 30);
-  });
-  renderer.refresh();
 }
 
 
@@ -81,60 +77,80 @@ function rotateGraph(graph, angle, center) {
 
 
 // main graph renderer
-const renderer = new Sigma(graph, document.getElementById("container"), { 
-  minCameraRatio: 0.9, maxCameraRatio: 1.5,
-  cameraPanBoundaries: { tolerance: 0, boundaries: { x: [-4, 4], y: [-3, 3] }},
-  defaultDrawNodeLabel: () => { return false; },
-  labelFont: 'cursive', labelSize: 24,
-  edgeProgramClasses: {
-    curved: EdgeCurveProgram,
-  },
-});
+window.addEventListener('load', () => {
+  const renderer = new Sigma(graph, document.getElementById("container"), { 
+    minCameraRatio: 0.9, maxCameraRatio: 1.5,
+    cameraPanBoundaries: { tolerance: 0, boundaries: { x: [-4, 4], y: [-3, 3] }},
+    defaultDrawNodeLabel: () => { return false; },
+    labelFont: 'Monospace', labelSize: 16,
+    edgeProgramClasses: {
+      curved: EdgeCurveProgram,
+    },
+  });
 
 
 
-// interaction
-let draggedNode = null;
-let isDragging = false;
+  // interaction
+  let draggedNode = null;
+  let isDragging = false;
+  let redirect = true;
 
-renderer.on("downNode", (e) => {
-  if (e.node === 'center') { return; }
-  isDragging = true;
-  draggedNode = e.node;
-  graph.setNodeAttribute(draggedNode, "highlighted", true);
-  if (!renderer.getCustomBBox()) {
-      renderer.setCustomBBox(renderer.getBBox());
-  }
-});
+  renderer.on("downNode", (e) => {
+    if (e.node === 'center') { return; }
+    isDragging = true;
+    draggedNode = e.node;
+    redirect = true;
 
-renderer.on("moveBody", ({ event }) => {
-  if (!isDragging || !draggedNode) { return };
-
-  const pos = renderer.viewportToGraph(event);
-  graph.setNodeAttribute(draggedNode, "x", pos.x);
-  graph.setNodeAttribute(draggedNode, "y", pos.y);
-
-  // Prevent sigma to move camera:
-  event.preventSigmaDefault();
-  event.original.preventDefault();
-  event.original.stopPropagation();
-});
-
-const handleUp = () => {
-    if (draggedNode) {
-        graph.removeNodeAttribute(draggedNode, "highlighted");
+    graph.setNodeAttribute(draggedNode, "highlighted", true);
+    if (!renderer.getCustomBBox()) {
+        renderer.setCustomBBox(renderer.getBBox());
     }
-    isDragging = false;
-    draggedNode = null;
-};
-renderer.on("upNode", handleUp);
-renderer.on("upStage", handleUp);
+  });
+
+  renderer.on("moveBody", ({ event }) => {
+    if (!isDragging || !draggedNode) { return };
+    redirect = false; // dont redirect if moving
+
+    const pos = renderer.viewportToGraph(event);
+    graph.setNodeAttribute(draggedNode, "x", pos.x);
+    graph.setNodeAttribute(draggedNode, "y", pos.y);
+
+    // Prevent sigma to move camera:
+    event.preventSigmaDefault();
+    event.original.preventDefault();
+    event.original.stopPropagation();
+  });
+
+  const handleUp = () => {
+      if (draggedNode) {
+        graph.removeNodeAttribute(draggedNode, "highlighted");
+
+        if (redirect) {
+          if (draggedNode === 'convention') {
+            window.open('convention.html', '_self');
+          } else if (draggedNode === 'insta') {
+            window.open('https://www.instagram.com/reparativecomputing/', '_blank');
+          } else if (draggedNode === 'horse') {
+            window.open('https://gradient.horse', '_blank');
+          } else if (draggedNode === 'orchard') {
+            window.open('https://docs.google.com/forms/d/e/1FAIpQLSfv3OifAdd2CCjsB6T4bzPlBcxBJPNNsBwlDGlOjcb-4VdiGA/viewform', '_blank');
+          }
+        }
+      }
+
+      redirect = false;
+      isDragging = false;
+      draggedNode = null;
+  };
+  renderer.on("upNode", handleUp);
+  renderer.on("upStage", handleUp);
 
 
-// rotate...
-function animate() {
-  rotateGraph(graph, ROTATION_SPEED, graphCenter);
-  renderer.refresh();
-  requestAnimationFrame(animate);
-}
-animate();
+  // rotate...
+  function animate() {
+    rotateGraph(graph, ROTATION_SPEED, graphCenter);
+    renderer.refresh();
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
--- orchard-web-convention-2026/src/mouse.js
+++ orchard-web-convention-2026/src/mouse.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', () => {
-    var characters = 'ABCDEFGHIJLNOPQRSTUVWXYZabcdefghijlmnopqrstuvwxyz0123456789';
+    var characters = 'ABCDEFGHIJOPQRSTUVWXYabcdefghijklmnopqrstuwxyz34567890=[{]}!+;:\'\"<>?';
     let char = characters.charAt(Math.floor(Math.random() * characters.length));
     document.getElementById('mouseLight').textContent = char;
 });
-    document.addEventListener('mousemove', (e) => {
+
+document.addEventListener('mousemove', (e) => {
     document.getElementById('mouseLight').style.left = (e.clientX - 15) + 'px';
     document.getElementById('mouseLight').style.top = (e.clientY - 15) + 'px';
 });
